12bf445cdc51aecb1cd2fe91978808bb73593f18
Jarmila Havířová <161177140+jarmilahavirova@users.noreply.github.com>
2024-05-28 19:26:57 +0200

    Nastavení dark/light mode

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 // https://dev.to/ananyaneogi/create-a-dark-light-mode-switch-with-css-variables-34l8
 
 const toggleSwitch = document.querySelector(
-  '.theme-switch input[type="checkbox"]',
+  '.theme-switch input[type="checkbox"]'
 );
 
 const switchTheme = (e) => {
   if (e.target.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark"); //storage
   } else {
-    document.documentElement.setAttribute('data-theme', 'light');
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light"); //storage
   }
 };
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener("change", switchTheme, false);
+
+//Store the user preference for future visits
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
